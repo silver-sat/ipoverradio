3f3d93047ae28e446e1f591a6a61532ca6c44773
Nathan Edwards <edwardsnj@users.noreply.github.com>
2020-02-13 23:29:16 -0500

    Update satellite_setup.sh

--- ipoverserial/satellite_setup.sh
+++ ipoverserial/satellite_setup.sh
@@ -1,14 +1,25 @@
 set -x
 cd /home/pi
+
+# install ax25 tools
 sudo apt install ax25-apps ax25-tools ntpsec
-sed -e '/^fi$/,/^exit 0$/{/^fi$/!d;};' /etc/rc.local > /tmp/rc.local
+
+# modify /etc/rc.local
+sed -e '/^fi$/,/^exit 0$/d' /etc/rc.local > /tmp/rc.local
+echo "fi" >> /tmp/rc.local
 wget -q -O - 'https://raw.githubusercontent.com/silver-sat/ipoverradio/master/ipoverserial/rc.local.insert.sh' >> /tmp/rc.local
 echo "exit 0" >> /tmp/rc.local
 sudo mv -f /tmp/rc.local /etc/rc.local
+sudo chown root.root /etc/rc.local
 sudo chmod +x /etc/rc.local
+
+# motify /etc/ax25/axports
 sed -e '/^serial/d' /etc/ax25/axports > /tmp/axports
 wget -q -O - 'https://raw.githubusercontent.com/silver-sat/ipoverradio/master/ipoverserial/axports.append.txt' >> /tmp/axports
 sudo mv -f /tmp/axports /etc/ax25/axports
+sudo chown root.root /etc/ax25/axports
+
+# Download and place appropriate startup script
 wget -O satellite_startup.sh 'https://raw.githubusercontent.com/silver-sat/ipoverradio/master/ipoverserial/satellite_startup.sh'
 rm -f .startup.sh
 ln -s satellite_startup.sh .startup.sh
