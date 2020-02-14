b1e1f3c06e17c4e025e9bbd20ca5e3ff36a40d2e
Nathan Edwards <edwardsnj@users.noreply.github.com>
2020-02-13 22:50:41 -0500

    Update satellite_setup.sh

--- ipoverserial/satellite_setup.sh
+++ ipoverserial/satellite_setup.sh
@@ -5,6 +5,7 @@ sed -e '/^fi$/,/^exit 0$/{/^fi$/!d;};' /etc/rc.local > /tmp/rc.local
 wget -q -O - 'https://raw.githubusercontent.com/silver-sat/ipoverradio/master/ipoverserial/rc.local.insert.sh' >> /tmp/rc.local
 echo "exit 0" >> /tmp/rc.local
 sudo mv -f /tmp/rc.local /etc/rc.local
+sudo chmod +x /etc/rc.local
 sed -e '/^serial/d' /etc/ax25/axports > /tmp/axports
 wget -q -O - 'https://raw.githubusercontent.com/silver-sat/ipoverradio/master/ipoverserial/axports.append.txt' >> /tmp/axports
 sudo mv -f /tmp/axports /etc/ax25/axports
