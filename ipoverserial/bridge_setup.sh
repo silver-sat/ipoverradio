b5d9dcb2a3916f6b35e0d10ef0298c1a13b206be
Nathan Edwards <edwardsnj@users.noreply.github.com>
2020-02-13 22:41:45 -0500

    Update bridge_setup.sh

--- ipoverserial/bridge_setup.sh
+++ ipoverserial/bridge_setup.sh
@@ -1,11 +1,13 @@
 set -x
 cd /home/pi
 sudo apt install ax25-apps ax25-tools ntpsec
-sudo sed -i -e '/^fi$/,/^exit 0$/{/^fi$/!d;};' /etc/rc.local
-sudo wget -q -O - 'https://raw.githubusercontent.com/silver-sat/ipoverradio/master/ipoverserial/rc.local.insert.sh' >> /etc/rc.local
-sudo echo "exit 0" >> /etc/rc.local
-sudo sed -i -e '/^serial/d' /etc/ax25/axports
-sudo wget -q -O - 'https://raw.githubusercontent.com/silver-sat/ipoverradio/master/ipoverserial/axports.append.txt' >> /etc/ax25/axports
+sed -e '/^fi$/,/^exit 0$/{/^fi$/!d;};' /etc/rc.local > /tmp/rc.local
+wget -q -O - 'https://raw.githubusercontent.com/silver-sat/ipoverradio/master/ipoverserial/rc.local.insert.sh' >> /tmp/rc.local
+echo "exit 0" >> /tmp/rc.local
+sudo mv -f /tmp/rc.local /etc/rc.local
+sed -e '/^serial/d' /etc/ax25/axports > /tmp/axports
+wget -q -O - 'https://raw.githubusercontent.com/silver-sat/ipoverradio/master/ipoverserial/axports.append.txt' >> /tmp/axports
+sudo mv -f /tmp/axports /etc/ax25/axports
 wget -O bridge_startup.sh 'https://raw.githubusercontent.com/silver-sat/ipoverradio/master/ipoverserial/bridge_startup.sh'
 rm -f .startup.sh
 ln -s bridge_startup.sh .startup.sh
