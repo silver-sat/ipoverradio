d3bd8cc5979bc4a748dbdd818fccd01f5fd1c8d2
Nathan Edwards <edwardsnj@users.noreply.github.com>
2020-03-21 10:58:24 -0400

    Update satellite_startup_ax25.sh

--- ipoverserial/satellite_startup_ax25.sh
+++ ipoverserial/satellite_startup_ax25.sh
@@ -7,14 +7,14 @@ set -x
 
 kissattach /dev/serial0 serial 192.168.100.102
 
-# Remove the default route if it is there, continue if not
+# Remove the default route via wlan0 if it is there, continue if not
 
-route del default || true
+# route del default || true
 
 # Add the default route via the /dev/ax0 device to 192.168.100.101 
 
-route add default gw 192.168.100.101 ax0
+# route add default gw 192.168.100.101 ax0
 
 # check if we can ping - turn off wifi if successful.
 
-ping -n -c 1 192.168.100.101 && rfkill block wifi
+# ping -n -c 1 192.168.100.101 && rfkill block wifi
